2b37af216a36bebcb7012b7383835a09879c8695
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2025-11-10 20:54:56 +0000

    fix kazakh keyboard

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,56 +1,13 @@
 export const WORDLE_MAX_ATTEMPTS = 6;
 
-/** Full Kazakh Cyrillic alphabet ordered for keyboard grouping. */
-const KAZAKH_ALPHABET = [
-  "а",
-  "ә",
-  "б",
-  "в",
-  "г",
-  "ғ",
-  "д",
-  "е",
-  "ё",
-  "ж",
-  "з",
-  "и",
-  "й",
-  "к",
-  "қ",
-  "л",
-  "м",
-  "н",
-  "ң",
-  "о",
-  "ө",
-  "п",
-  "р",
-  "с",
-  "т",
-  "у",
-  "ұ",
-  "ү",
-  "ф",
-  "х",
-  "һ",
-  "ц",
-  "ч",
-  "ш",
-  "щ",
-  "ы",
-  "і",
-  "ь",
-  "ъ",
-  "э",
-  "ю",
-  "я",
-];
-
 export type KeyboardKey = string | "enter" | "backspace";
 
+/** Physical Kazakh (JCUKEN) keyboard layout, top to bottom. */
 export const KAZAKH_KEYBOARD_ROWS: KeyboardKey[][] = [
-  KAZAKH_ALPHABET.slice(0, 14),
-  KAZAKH_ALPHABET.slice(14, 28),
-  ["enter", ...KAZAKH_ALPHABET.slice(28), "backspace"],
+  ["Ә", "І", "Ң", "Ғ", "Ү", "Ұ", "Қ", "Ө", "Һ", "backspace"],
+  ["Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х"],
+  ["Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э"],
+  ["Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", "enter"],
 ];
+
 
